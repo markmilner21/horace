04d118a0b9776fb1427723243f69bb3f50a1df3c
Mark Milner <ra21087@bristol.ac.uk>
2026-04-15 18:55:56 +0100

    horace: add inital prompts file

---
+++ horace/prompts.py
@@ -0,0 +1,6 @@
+QUESTIONS = [
+    "Hi, I'm Horace. What is your name?",
+    "Nice to meet you. What do you do?",
+    "What are you trying to build right now?",
+    "Why is this important to you?"
+]
