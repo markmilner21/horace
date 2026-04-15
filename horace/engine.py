c516d2dd0c64bd40ebd660b059a9a2b1fcf2c5a1
Mark Milner <ra21087@bristol.ac.uk>
2026-04-15 18:57:24 +0100

    horace: Establish Engine Class
    This is the original Horace class with 2 attributes
    for storing the response and for keeping an index.
    the index determines which question is being asked.

---
+++ horace/engine.py
@@ -0,0 +1,18 @@
+from horace.prompts import QUESTIONS
+
+class HoraceEngine:
+    def __init__(self):
+        self.answers = []
+        self.index = 0
+
+    def has_next_question(self):
+        return self.index < len(QUESTIONS)
+
+    def get_next_question(self):
+        if self.has_next_question():
+            return QUESTIONS[self.index]
+        return None
+
+    def submit_answer(self, answer: str):
+        self.answers.append(answer)
+        self.index += 1
